9986175930a7ecfeb51c707fdc55cb78a9d4fd3c
Erik Steenman <eriksteenman@gmail.com>
2016-02-26 16:28:50 +0100

    Updated package files.

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ module = __import__(module_name)
 with open('README.rst') as readme:
     long_description = readme.read()
 
+with open('requirements.txt') as requirements:
+    install_requires = requirements.readlines()
+
 setup(
     name=package_name,
     version=module.__version__,
@@ -15,7 +18,7 @@ setup(
     long_description=long_description,
     author='Authors of %s, see AUTHORS' % module.__verbose_name__,
     author_email='eriksteenman@gmail.com',
-    url='http://steenman.me',
+    url='http://github.com/lesteenman',
     keywords='OpenSlides',
     classifiers=[
         'Development Status :: 1 - Production/Unstable',
@@ -26,7 +29,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2'],
     license='MIT',
-    packages=find_packages(exclude=['tests']),
+    packages=find_packages(),
     include_package_data=True,
     install_requires=install_requires,
     entry_points={'openslides_plugins': '%s = %s' % (module.__verbose_name__, module_name)})
