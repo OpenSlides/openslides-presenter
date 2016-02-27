ff05a68fa2676eedd5c04b11950e467331bcae3a
Erik Steenman <eriksteenman@gmail.com>
2016-02-27 09:25:15 +0100

    Add page up and page down to controls.

--- openslides_clicker/static/js/clicker.js
+++ openslides_clicker/static/js/clicker.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
     $('body').click(nextSlide);
     $('iframeoverlay').click(nextSlide);
     $(document).keyup(function(e) {
-        if (e.which == 37 || e.which == 40) previousSlide();
-        else if (e.which == 39 || e.which == 38) nextSlide();
+        if (e.which == 37 || e.which == 40 || e.which == 33) previousSlide();
+        else if (e.which == 39 || e.which == 38 || e.which == 34) nextSlide();
     });
 });
