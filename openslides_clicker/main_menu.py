cd23cd301d8ea689baef305e4670870b52b6d021
Erik Steenman <eriksteenman@gmail.com>
2016-02-28 10:06:56 +0100

    Add custom permission needed for the 'clicker'.
    Useful to set up a presenter user that you can log in to on a
    presentation computer, if you do not want to allow that user to actually
    change the currently shown presentation.

--- openslides_clicker/main_menu.py
+++ openslides_clicker/main_menu.py
@@ -5,7 +5,7 @@ class OpenslidesClickerPluginMainMenuEntry(MainMenuEntry):
     Main menu entry for OpenSlides Clicker Plugin.
     """
     verbose_name = 'PDF Remote'
-    required_permission = 'core.can_manage_projector'
+    required_permission = 'openslides_clicker.can_manage_current_presentation'
     default_weight = 100
     pattern_name = 'openslides_clicker'
     icon_css_class = 'icon-hand-up'
