ed7c0f6218120a8736646bfe18dddefa60537b2b
Emanuel Schütze <emanuel@intevation.de>
2018-06-22 14:32:03 +0200

    Prepare 2.0.3 release

--- openslides_presenter/__init__.py
+++ openslides_presenter/__init__.py
@@ -1,5 +1,7 @@
 __verbose_name__ = 'OpenSlides Presenter'
 __description__ = 'This plugin for OpenSlides provides a much improved presentation interface.'
-__version__ = '2.0.3'
+__version__ = '2.0.3-dev'
+__license__ = 'MIT'
+__url__ = 'https://github.com/OpenSlides/openslides-presenter'
 
 default_app_config = 'openslides_presenter.apps.PresenterAppConfig'
--- openslides_presenter/apps.py
+++ openslides_presenter/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
 
-from . import __description__, __verbose_name__, __version__
+from . import (
+    __description__,
+    __license__,
+    __url__,
+    __verbose_name__,
+    __version__,
+)
 
 
 class PresenterAppConfig(AppConfig):
@@ -8,6 +14,8 @@ class PresenterAppConfig(AppConfig):
     verbose_name = __verbose_name__
     description = __description__
     version = __version__
+    license = __license__
+    url = __url__
     angular_site_module = True
     angular_projector_module = False
     js_files = [
